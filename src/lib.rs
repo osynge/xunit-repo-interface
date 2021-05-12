2caa6eb349a8979c4785666f76f083cfe42eb63a
Owen Synge <osynge@googlemail.com>
2021-05-12 15:31:38 +0200

    derive clone

--- src/lib.rs
+++ src/lib.rs
@@ -6,33 +6,33 @@ pub use serde::{Deserialize, Serialize};
 pub use xunit_struct::errors::XunitError;
 pub use xunit_struct::model::Xunit;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct File {
     pub content: Xunit,
     pub directory: String,
     pub filename: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Environment {
     pub sk: Option<String>,
     pub key_value: HashMap<String, String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Project {
     pub sk: Option<String>,
     pub identifier: Option<String>,
     pub human_name: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Run {
     pub sk: Option<String>,
     pub client_identifier: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Upload {
     pub project: Project,
     pub environment: Environment,
@@ -40,7 +40,7 @@ pub struct Upload {
     pub files: Vec<File>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct UploadResponse {
     pub project: String,
     pub environment: String,
